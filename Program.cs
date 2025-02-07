4521a2f7d0f97d8c5d36ae187908fa353a26b40e
raddevus <1radeutsch1@gmail.com>
2025-02-07 16:44:21 -0500

    Added async method to get bmp width.

--- Program.cs
+++ Program.cs
@@ -11,3 +11,15 @@ na.GetBitmapWidth(args[0]);
 
 asynctargets at = new();
 at.ReadBytesGetCount(args[0]);
+
+
+var readCompleted = at.GetBitmapWidth("test.bmp"); 
+readCompleted.ContinueWith(t => 
+    { 
+        if (t.IsCompletedSuccessfully) 
+        { 
+            Console.WriteLine($"I'm done.{DateTime.Now} - width is: {readCompleted.Result}"); 
+            return; 
+        } 
+        Console.WriteLine("got here..."); 
+    });
--- asynctargets.cs
+++ asynctargets.cs
@@ -16,4 +16,21 @@ class asynctargets{
                 Console.WriteLine($"I couldn't do that. {t.Exception.InnerExceptions[0].Message}"); 
             }); 
     }
+
+    public async Task<int> GetBitmapWidth(string path)
+    {
+        using (var file = new FileStream(path, FileMode.Open, FileAccess.Read))
+        {
+            var fileId = new byte[2];
+            var read = await file.ReadAsync(fileId, 0, 2);
+            if (read != 2 || fileId[0] != 'B' || fileId[1] != 'M')
+            throw new Exception("Not a BMP file");
+            
+            file.Seek(18, SeekOrigin.Begin);
+            var widthBuffer = new byte[4];
+            read = await file.ReadAsync(widthBuffer, 0, 4);
+            if(read != 4) throw new Exception("Not a BMP file");
+            return BitConverter.ToInt32(widthBuffer, 0);
+        }
+    }
 }
